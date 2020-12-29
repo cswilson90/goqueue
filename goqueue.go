a84033b4d3f97579361657486f105d3c0b24c0c4
Chris Wilson <cswilson90@gmail.com>
2020-12-29 14:25:32 +0000

    Run a server in the main executable

--- goqueue.go
+++ goqueue.go
@@ -1,7 +1,16 @@
-package goqueue
+package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/cswilson90/goqueue/internal/server"
+)
 
 func main() {
-	fmt.Println("Not implemented")
+	server, err := server.NewGoJobServer("localhost", "11223")
+	if err != nil {
+		log.Fatal("Failed to create server: " + err.Error())
+	}
+
+	server.Run()
 }
